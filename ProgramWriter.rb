28323a63d57779abf8c67b2380cc90671e8b188c
TechBug2012 <mattbstanciu@gmail.com>
2017-04-13 19:55:06 -0400

    init

---
+++ ProgramWriter.rb
@@ -0,0 +1,16 @@
+file = File.open('program.rb', 'w')
+File.readlines("instructions.txt").each do |line|
+   puts "#{line}"
+   if line.include? "integer"
+      line_string = line.to_s
+      split[] = line_string.split(' ')
+      var_index = split.index("called") + 1
+      var_name = split.at(var_index)
+      value_index = split.index("value") + 1
+      value = split.at(value_index)
+
+      file.write("#{var_name} = #{value}")
+   end
+   
+end
+file.close
