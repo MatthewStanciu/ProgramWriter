a3cb65d273258886c4ac621d644608ebcdc10895
TechBug2012 <mattbstanciu@gmail.com>
2017-04-13 20:10:42 -0400

    removed line_string variable

--- ProgramWriter.rb
+++ ProgramWriter.rb
@@ -3,8 +3,7 @@ file.write("#generated file")
 File.readlines("instructions.txt").each do |line|
    puts "#{line}"
    if line.include? "integer"
-      line_string = line.to_s
-      split = line_string.split(' ')
+      split = line.split(' ')
       var_index = split.index("called") + 1
       var_name = split.at(var_index)
       value_index = split.index("value") + 1
