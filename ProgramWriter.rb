04771969613e5ab6b39c54ee88bf0b56e6712471
TechBug2012 <mattbstanciu@gmail.com>
2017-04-13 20:07:42 -0400

    works for integer!

--- ProgramWriter.rb
+++ ProgramWriter.rb
@@ -4,13 +4,13 @@ File.readlines("instructions.txt").each do |line|
    puts "#{line}"
    if line.include? "integer"
       line_string = line.to_s
-      split[] = line_string.split(' ')
+      split = line_string.split(' ')
       var_index = split.index("called") + 1
       var_name = split.at(var_index)
       value_index = split.index("value") + 1
       value = split.at(value_index)
 
-      file.write("#{var_name} = #{value}")
+      file.write("\n#{var_name} = #{value}")
    end
    
 end
--- program.rb
+++ program.rb
@@ -1,1 +1,2 @@
 #generated file
+test = 5
